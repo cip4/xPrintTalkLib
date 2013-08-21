039a0bf73bf3efa0a93ae728c330b0140971680c
Stefan Meissner <s.meissner@flyeralarm.de>
2013-08-21 11:23:12 +0000

    Unpackager now supports single file extraction and master document extraction.
    git-svn-id: http://svn.cip4.org/svnroot/trunk/xPrintTalkLib@6305 25ee8a6d-831b-0410-907a-ff8528d65d49

---
+++ src/test/java/org/cip4/lib/xprinttalk/xml/PrintTalkUnpackagerTest.java
@@ -0,0 +1,136 @@
+/**
+ * All rights reserved by
+ * 
+ * flyeralarm GmbH
+ * Alfred-Nobel-Straße 18
+ * 97080 Würzburg
+ *
+ * Email: info@flyeralarm.com
+ * Website: http://www.flyeralarm.com
+ */
+package org.cip4.lib.xprinttalk.xml;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * JUnit test case for PrintTalkUnpackager class.
+ * @author s.meissner
+ * @date 21.08.2013
+ */
+public class PrintTalkUnpackagerTest {
+
+	private final static String RES_PGK = "/org/cip4/lib/xprinttalk/testPackage.zip";
+
+	/**
+	 * Setup unit test.
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Tear down unit test.
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link org.cip4.lib.xjdf.xml.internal.AbstractXmlUnpackager#unpackageZip(java.lang.String, java.lang.String)}.
+	 * @throws IOException
+	 */
+	@Test
+	public void testUnpackageZip() throws IOException {
+
+		// arrange
+		String pathZip = PrintTalkUnpackagerTest.class.getResource(RES_PGK).getFile();
+
+		String tmpDir = FileUtils.getTempDirectoryPath();
+		String targetDir = FilenameUtils.concat(tmpDir, "xPrintTalkLib_UnpackageTest_" + System.currentTimeMillis());
+
+		// assert
+		PrintTalkUnpackager unpackager = new PrintTalkUnpackager(pathZip);
+		unpackager.unpackagePrintTalk(targetDir);
+
+		// act
+		File targetFile = new File(targetDir);
+		Assert.assertTrue("Target destination does not exist.", targetFile.exists());
+
+		String pathPtk = FilenameUtils.concat(targetDir, "testPackage.ptk");
+		Assert.assertTrue("File does not exist.", new File(pathPtk).exists());
+
+		String pathArtwork = FilenameUtils.concat(targetDir, "artwork/testArtwork.pdf");
+		Assert.assertTrue("Artwork does not exist.", new File(pathArtwork).exists());
+
+		String pathPreview = FilenameUtils.concat(targetDir, "preview/testPreview.pdf");
+		Assert.assertTrue("Preview does not exist.", new File(pathPreview).exists());
+
+		FileUtils.deleteDirectory(targetFile); // clean up
+		Assert.assertFalse("Target destination was not removed.", targetFile.exists());
+	}
+
+	/**
+	 * Test method for {@link org.cip4.lib.xjdf.xml.internal.AbstractXmlUnpackager#unpackageZip(java.lang.String, java.lang.String)}.
+	 * @throws IOException
+	 */
+	@Test
+	public void testUnpackageZipTempDir() throws IOException {
+
+		// arrange
+		String pathZip = PrintTalkUnpackagerTest.class.getResource(RES_PGK).getFile();
+
+		// assert
+		PrintTalkUnpackager unpackager = new PrintTalkUnpackager(pathZip);
+		String targetDir = unpackager.unpackagePrintTalk();
+
+		// act
+		File targetFile = new File(targetDir);
+		Assert.assertTrue("Target destination does not exist.", targetFile.exists());
+
+		String pathPtk = FilenameUtils.concat(targetDir, "testPackage.ptk");
+		Assert.assertTrue("File does not exist.", new File(pathPtk).exists());
+
+		String pathArtwork = FilenameUtils.concat(targetDir, "artwork/testArtwork.pdf");
+		Assert.assertTrue("Artwork does not exist.", new File(pathArtwork).exists());
+
+		String pathPreview = FilenameUtils.concat(targetDir, "preview/testPreview.pdf");
+		Assert.assertTrue("Preview does not exist.", new File(pathPreview).exists());
+
+		FileUtils.deleteDirectory(targetFile); // clean up
+		Assert.assertFalse("Target destination was not removed.", targetFile.exists());
+	}
+
+	/**
+	 * Test method for {@link org.cip4.lib.xjdf.xml.XJdfPackageManager#getXJdfDocument()}.
+	 * @throws FileNotFoundException
+	 */
+	@Test
+	public void testGetPrintTalkDocument() throws Exception {
+
+		// arrange
+		String path = PrintTalkUnpackagerTest.class.getResource(RES_PGK).getFile();
+		PrintTalkUnpackager unpackager = new PrintTalkUnpackager(path);
+
+		// act
+		PrintTalkNavigator nav = unpackager.getPrintTalkDocument();
+
+		String pathPreview = nav.evaluateString("//XJDF/ParameterSet[@Name='Preview']/Parameter/Preview/@URL");
+		byte[] bytes = unpackager.extractFile(pathPreview);
+
+		// assert
+		Assert.assertEquals("BusinessID is wrong.", "95733854", nav.evaluateString(PrintTalkNavigator.BUSINESS_ID));
+		Assert.assertNotEquals("Preview is null.", 0, bytes.length);
+	}
+
+}
