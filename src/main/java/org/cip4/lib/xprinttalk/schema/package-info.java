937c33ed65c2639b3fc25e028a0d8dc08d2c7565
Michel Hartmann <m.hartmann@flyeralarm.de>
2014-03-06 15:16:46 +0000

    Fixed generation of java sources
    git-svn-id: http://svn.cip4.org/svnroot/trunk@6692 25ee8a6d-831b-0410-907a-ff8528d65d49

--- src/main/java/org/cip4/lib/xprinttalk/schema/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.printtalk.org/schema_2_0", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.cip4.lib.xprinttalk.schema;
