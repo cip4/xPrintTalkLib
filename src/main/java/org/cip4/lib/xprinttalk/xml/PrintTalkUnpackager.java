73ff7d04a53937ef99a5e17fd598896a5afc226e
Michel Hartmann <M.Hartmann@flyeralarm.de>
2015-01-29 19:02:40 +0100

    Added constructor to read PrintTalk-Package from InputStream

--- src/main/java/org/cip4/lib/xprinttalk/xml/PrintTalkUnpackager.java
+++ src/main/java/org/cip4/lib/xprinttalk/xml/PrintTalkUnpackager.java
@@ -1,13 +1,3 @@
-/**
- * All rights reserved by
- *
- * flyeralarm GmbH
- * Alfred-Nobel-Straße 18
- * 97080 Würzburg
- *
- * Email: info@flyeralarm.com
- * Website: http://www.flyeralarm.com
- */
 package org.cip4.lib.xprinttalk.xml;
 
 import java.io.IOException;
@@ -16,86 +6,91 @@ import org.cip4.lib.xjdf.xml.internal.AbstractXmlUnpackager;
 
 /**
  * Unpackaging logic for PrintTalk Packages.
+ *
  * @author s.meissner
- * @date 12.08.2013
+ * @author m.hartmann
  */
 public class PrintTalkUnpackager extends AbstractXmlUnpackager {
 
-	/**
-	 * Custom constructor. Accepting a PrintTalk Package Path for initializing.
-	 * @param pathPackage Path to XJDF Package.
-	 * @throws IOException
-	 */
-	public PrintTalkUnpackager(String pathPackage) throws IOException {
-		super(pathPackage);
-	}
+    /**
+     * Custom constructor. Accepting a PrintTalk Package Path for initializing.
+     *
+     * @param pathPackage Path to XJDF Package.
+     *
+     * @throws IOException if an I/O error occurs.
+     */
+    public PrintTalkUnpackager(final String pathPackage) throws IOException {
+        super(pathPackage);
+    }
 
-	/**
-	 * Returns the PrintTalk Master Document covered by the PrintTalkNavigator.
-	 * @return PrintTalk Document covered by the PrintTalkNavigator.
-	 * @throws Exception
-	 */
-	public PrintTalkNavigator getPrintTalkDocument() throws Exception {
+    /**
+     * Returns the PrintTalk Master Document covered by the PrintTalkNavigator.
+     *
+     * @return PrintTalk Document covered by the PrintTalkNavigator.
+     * @throws Exception
+     */
+    public final PrintTalkNavigator getPrintTalkDocument() throws Exception {
+        return getPrintTalkDocument(false);
+    }
 
-		return getPrintTalkDocument(false);
-	}
+    /**
+     * Returns the PrintTalk Master Document covered by the PrintTalkNavigator.
+     *
+     * @param namespaceAware true if document should be opened namespace aware.
+     *
+     * @return PrintTalk Document covered by the PrintTalkNavigator.
+     * @throws Exception
+     */
+    public final PrintTalkNavigator getPrintTalkDocument(final boolean namespaceAware) throws Exception {
+        // find XJDF master document
+        byte[] bytes = super.findMasterDocument();
 
-	/**
-	 * Returns the PrintTalk Master Document covered by the PrintTalkNavigator.
-	 * @return PrintTalk Document covered by the PrintTalkNavigator.
-	 * @throws Exception
-	 */
-	public PrintTalkNavigator getPrintTalkDocument(boolean namespaceAware) throws Exception {
+        // return as navigator
+        return new PrintTalkNavigator(bytes, namespaceAware);
+    }
 
-		// find XJDF master document
-		byte[] bytes = super.findMasterDocument();
+    /**
+     * Unpackage an PrintTalk Package to a temporary directory.
+     *
+     * @return The path of the printtalk document.
+     * @throws IOException if an I/O error occurs.
+     */
+    public final String unpackagePrintTalkTemp() throws IOException {
+        // unpackage
+        return super.unpackageZipTemp();
+    }
 
-		// return as navigator
-		return new PrintTalkNavigator(bytes, namespaceAware);
-	}
+    /**
+     * Unpackage an PrintTalk Package to a temporary directory.
+     *
+     * @param appName Name of the app.
+     *
+     * @return The path of the printtalk document.
+     * @throws IOException if an I/O error occurs.
+     */
+    public final String unpackagePrintTalkTemp(final String appName) throws IOException {
+        // unpackage
+        return super.unpackageZipTemp(appName);
+    }
 
-	/**
-	 * Unpackage an PrintTalk Package to a temporary directory.
-	 * @param pathPackage PrintTalk Package to be unpackaged.
-	 * @return The path of the printtalk document.
-	 * @throws IOException
-	 */
-	public String unpackagePrintTalkTemp() throws IOException {
+    /**
+     * Unpackage an PrintTalk Package to a target directory.
+     *
+     * @param targetDir Target directory for unpackaging.
+     *
+     * @return The path of the printtalk document.
+     * @throws IOException if an I/O error occurs.
+     */
+    public final String unpackagePrintTalk(final String targetDir) throws IOException {
+        // unpackge
+        return super.unpackageZip(targetDir);
+    }
 
-		// unpackage
-		return super.unpackageZipTemp();
-	}
-
-	/**
-	 * Unpackage an PrintTalk Package to a temporary directory.
-	 * @param pathPackage PrintTalk Package to be unpackaged.
-	 * @return The path of the printtalk document.
-	 * @throws IOException
-	 */
-	public String unpackagePrintTalkTemp(String appName) throws IOException {
-
-		// unpackage
-		return super.unpackageZipTemp(appName);
-	}
-
-	/**
-	 * Unpackage an PrintTalk Package to a target directory.
-	 * @param pathPackage PrintTalk Package to be unpackaged.
-	 * @param targetDir Target directory for unpackaging.
-	 * @return The path of the printtalk document.
-	 * @throws IOException
-	 */
-	public String unpackagePrintTalk(String targetDir) throws IOException {
-
-		// unpackge
-		return super.unpackageZip(targetDir);
-	}
-
-	/**
-	 * @see org.cip4.lib.xjdf.xml.internal.AbstractXmlUnpackager#getMasterExtension()
-	 */
-	@Override
-	protected String[] getMasterExtension() {
-		return new String[] { "ptk", "xjdf" };
-	}
+    /**
+     * @see org.cip4.lib.xjdf.xml.internal.AbstractXmlUnpackager#getMasterExtension()
+     */
+    @Override
+    protected final String[] getMasterExtension() {
+        return new String[]{"ptk", "xjdf"};
+    }
 }
