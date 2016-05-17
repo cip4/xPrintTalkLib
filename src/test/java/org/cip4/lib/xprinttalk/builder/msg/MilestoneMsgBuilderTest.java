6667f5a1f04dfae833462d3a2106eecf1b4600c4
j.hammett <Jeffrey.Hammett@flyeralarm.com>
2016-05-17 14:42:24 +0200

    Revert "Fixed changes introduced by xJdfLib"
    This (PARTIALLY) reverts commit 48a53577aa67bd64b91ccba864fd3a6ae4937b2f.

--- src/test/java/org/cip4/lib/xprinttalk/builder/msg/MilestoneMsgBuilderTest.java
+++ src/test/java/org/cip4/lib/xprinttalk/builder/msg/MilestoneMsgBuilderTest.java
@@ -1,26 +1,15 @@
-/**
- * All rights reserved by
- * 
- * flyeralarm GmbH
- * Alfred-Nobel-Straße 18
- * 97080 Würzburg
- *
- * Email: info@flyeralarm.com
- * Website: http://www.flyeralarm.com
- */
 package org.cip4.lib.xprinttalk.builder.msg;
 
 import org.cip4.lib.xprinttalk.builder.AbstractBuilderTest;
 import org.cip4.lib.xprinttalk.schema.PrintTalk;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 /**
  * JUnit Test Case for MilestoneBuilder
- * @author stefan.meissner
- * @date 06.12.2012
  */
 public class MilestoneMsgBuilderTest extends AbstractBuilderTest<PrintTalk> {
 
@@ -50,19 +39,22 @@ public class MilestoneMsgBuilderTest extends AbstractBuilderTest<PrintTalk> {
 
 		// arrange
 		final String BUSINESS_ID = "businessID";
+		final String JOB_ID = "jobID";
 		final String MILESTONE_TYPE = "milestoneType";
 
 		// act
-		MilestoneMsgBuilder builder = new MilestoneMsgBuilder(BUSINESS_ID, MILESTONE_TYPE);
+		MilestoneMsgBuilder builder = new MilestoneMsgBuilder(BUSINESS_ID, JOB_ID, MILESTONE_TYPE);
 
 		// assert
 		byte[] bytes = marsahlResult(builder);
 
 		String actualBusinessID = getXPathValue(bytes, "/ptk:PrintTalk/ptk:Request/ptk:OrderStatusResponse/@BusinessID");
-		Assert.assertEquals("BusinessID is wrong.", BUSINESS_ID, actualBusinessID);
+		assertEquals("BusinessID is wrong.", BUSINESS_ID, actualBusinessID);
+
+		String actualJobID = getXPathValue(bytes, "/ptk:PrintTalk/ptk:Request/ptk:OrderStatusResponse/xjdf:Notification/@JobID");
+		assertEquals("JobID is wrong.", JOB_ID, actualJobID);
 
 		String actualMilestoneType = getXPathValue(bytes, "/ptk:PrintTalk/ptk:Request/ptk:OrderStatusResponse/xjdf:Notification/xjdf:Milestone/@MilestoneType");
-		Assert.assertEquals("MilestoneType is wrong.", MILESTONE_TYPE, actualMilestoneType);
+		assertEquals("MilestoneType is wrong.", MILESTONE_TYPE, actualMilestoneType);
 	}
-
 }
