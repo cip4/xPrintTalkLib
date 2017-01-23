625f3d2c97d410454d30250fa0aa427bc35cea58
Michel Hartmann <M.Hartmann@flyeralarm.de>
2017-01-23 15:24:51 +0100

    Fixed build that got broken due to changes on the xJdfLib and the schema

--- src/main/java/org/cip4/lib/xprinttalk/builder/msg/MilestoneMsgBuilder.java
+++ src/main/java/org/cip4/lib/xprinttalk/builder/msg/MilestoneMsgBuilder.java
@@ -1,11 +1,8 @@
 package org.cip4.lib.xprinttalk.builder.msg;
 
 import org.cip4.lib.xjdf.XJdfNodeFactory;
-import org.cip4.lib.xjdf.schema.EnumClass;
-import org.cip4.lib.xjdf.schema.EnumType;
 import org.cip4.lib.xjdf.schema.Milestone;
 import org.cip4.lib.xjdf.schema.Notification;
-import org.cip4.lib.xjdf.type.DateTime;
 import org.cip4.lib.xprinttalk.PrintTalkNodeFactory;
 import org.cip4.lib.xprinttalk.builder.PrintTalkBuilder;
 import org.cip4.lib.xprinttalk.schema.OrderStatusResponse;
@@ -35,8 +32,8 @@ public final class MilestoneMsgBuilder extends PrintTalkBuilder {
 
 		Notification notification = nf.createNotification();
 		notification.setJobID(jobID);
-        notification.setClazz(EnumClass.EVENT);
-        notification.setType(EnumType.MILESTONE);
+        notification.setClazz(Notification.Class.EVENT);
+        notification.setType(Notification.Type.MILESTONE);
 		notification.setMilestone(milestone);
 
 		OrderStatusResponse orderStatusResponse = ptkNf.createOrderStatusResponse(businessID);
